0b3c0f66e0dad4b0b3f8d6b21ea462b91c7e0404
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-04-26 16:35:26 +0100

    Emulate key press in ApiSettingsForm test
    Passing a state object is wrong, we should be emulating the user

--- src/settings/ApiSettingsForm.test.js
+++ src/settings/ApiSettingsForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
-import { act, screen } from '@testing-library/react';
+import { act, screen, fireEvent } from '@testing-library/react';
 import { StripesContext } from '@folio/stripes-core/src/StripesContext';
 import renderWithIntl from '../../test/jest/helpers/renderWithIntl';
 
@@ -17,9 +17,7 @@ const stripesStub = {
 
 const renderForm = (extraProps) => {
   const formProps = {
-    label: 'Test',
     onSubmit: () => {},
-    values: {},
     ...extraProps
   };
 
@@ -41,7 +39,11 @@ describe('ApiSettingsForm', () => {
     expect(select).not.toBeInTheDocument();
   });
   test('does render LicenceChooser when apiKey is passed', () => {
-    renderForm({ values: { apiKey: '123' } });
+    renderForm();
+    const input = screen.getByLabelText('Enter your CLA API key');
+    fireEvent.keyDown(input, { key: 'a', keyCode: 65 });
+    fireEvent.keyDown(input, { key: 'b', keyCode: 66 });
+    fireEvent.keyDown(input, { key: 'c', keyCode: 67 });
     const select = screen.queryByRole('combobox');
     expect(select).toBeInTheDocument();
   });
