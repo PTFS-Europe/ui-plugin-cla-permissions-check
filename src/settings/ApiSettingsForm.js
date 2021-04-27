PTFS-Europe/ui-plugin-cla-permissions-check

commit 657c1a9bc53a716ee6b35f9efbd65d665b101095

Rename component and handleSubmit -> onSubmit
When creating unit tests I noticed that the component was misnamed when
compared with the filename and how it was imported, this was fixed.

Also, the handleSubmit function that is passed as a prop needs to be
called onSubmit otherwise tests fail

src/settings/ApiSettingsForm.js
@@ -13,14 +13,14 @@ import stripesFinalForm from '@folio/stripes/final-form';
 
 import LicenceChooser from './LicenceChooser';
 
-const DisplayApiSettingsForm = ({
-  handleSubmit,
+const ApiSettingsForm = ({
+  onSubmit,
   label,
   pristine,
   submitting,
   values
 }) => (
-  <form id="cla-api-settings-form" onSubmit={handleSubmit}>
+  <form id="cla-api-settings-form" onSubmit={onSubmit}>
     <Pane
       defaultWidth="fill"
       fluidContentWidth
@@ -52,8 +52,8 @@ const DisplayApiSettingsForm = ({
   </form>
 );
 
-DisplayApiSettingsForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
+ApiSettingsForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
   label: PropTypes.node,
   pristine: PropTypes.bool,
   submitting: PropTypes.bool,
@@ -68,4 +68,4 @@ export default stripesFinalForm({
   subscription: {
     values: true
   }
-})(DisplayApiSettingsForm);
+})(ApiSettingsForm);

commit fdf997a7c339369d8eb5f3e54da5236525729090

onSubmit -> handleSubmit
The change in 657c1a9bc53a716ee6b35f9efbd65d665b101095 broke settings
savings. Despite rooting around in stripes-connect and stripes-core,
I've not been able to determine why ApiSettingsForm is passed both a
handleSubmit function and a onSubmit function and I've not been able to
determine the distinction between the two. So, I'm happy at this point
to use handleSubmit as it restores the saving functionality and passes
the tests.

src/settings/ApiSettingsForm.js
@@ -14,13 +14,13 @@ import stripesFinalForm from '@folio/stripes/final-form';
 import LicenceChooser from './LicenceChooser';
 
 const ApiSettingsForm = ({
-  onSubmit,
+  handleSubmit,
   label,
   pristine,
   submitting,
   values
 }) => (
-  <form id="cla-api-settings-form" onSubmit={onSubmit}>
+  <form id="cla-api-settings-form" onSubmit={handleSubmit}>
     <Pane
       defaultWidth="fill"
       fluidContentWidth
@@ -53,7 +53,7 @@ const ApiSettingsForm = ({
 );
 
 ApiSettingsForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
   label: PropTypes.node,
   pristine: PropTypes.bool,
   submitting: PropTypes.bool,