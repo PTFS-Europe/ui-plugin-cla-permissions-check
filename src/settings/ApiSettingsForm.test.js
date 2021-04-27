a0f1ffb8ce8e5cf4cb6b681b373183252c28e504
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-04-27 11:43:31 +0100

    Complete ApiSettingsForm tests
    A switch to a different method of performing keyboard input (as
    recommended here https://testing-library.com/docs/example-input-event/)
    seems to have fixed the problem with the input value not being updated.

--- src/settings/ApiSettingsForm.test.js
+++ src/settings/ApiSettingsForm.test.js
@@ -15,10 +15,9 @@ const stripesStub = {
   plugins: {},
 };
 
-const renderForm = (extraProps) => {
+const renderForm = () => {
   const formProps = {
-    onSubmit: () => {},
-    ...extraProps
+    onSubmit: () => {}
   };
 
   act(() => {
@@ -32,19 +31,23 @@ const renderForm = (extraProps) => {
   });
 };
 
+// We check for elements with an 'alert' role because <LicenceChooser>
+// renders a spinner until it gets an API response.
+// We test for the length to be 1 because react-final-form creates an element
+// after the <input> which has a role of alert.
 describe('ApiSettingsForm', () => {
   test('does not render LicenceChooser when no apiKey is passed', () => {
     renderForm();
-    const select = screen.queryByRole('combobox');
-    expect(select).not.toBeInTheDocument();
+    const noSpinner = screen.queryAllByRole('alert');
+    expect(noSpinner).toHaveLength(1);
   });
-  test('does render LicenceChooser when apiKey is passed', () => {
+  // We test for the length to be 2 because react-final-form creates an element
+  // after the <input> which has a role of alert.
+  test('does render LicenceChooser when apiKey is passed', async () => {
     renderForm();
     const input = screen.getByLabelText('Enter your CLA API key');
-    fireEvent.keyDown(input, { key: 'a', keyCode: 65 });
-    fireEvent.keyDown(input, { key: 'b', keyCode: 66 });
-    fireEvent.keyDown(input, { key: 'c', keyCode: 67 });
-    const select = screen.queryByRole('combobox');
-    expect(select).toBeInTheDocument();
+    fireEvent.change(input, { target: { value: '123' } });
+    const spinner = await screen.queryAllByRole('alert');
+    expect(spinner).toHaveLength(2);
   });
 });
